59f5663078f96ebd1875e0780987aeb11cf4cec0
brachi <40743558+brachiGH@users.noreply.github.com>
2020-05-17 00:02:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var request = require('request');
+
+var headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', //<-- :D
+    'Authorization': 'Basic ODRh...wNGE='
+};
+
+var dataString = 'grant_type=refresh_token&refresh_token=AQCF....A3hU';
+
+var options = {
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+request(options, function(error, response, body) {
+  console.log(body);
+});
